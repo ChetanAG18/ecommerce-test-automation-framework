7cd66394fdfb660b65ede36bed618aaaef43d116
Chetan AG <agchetan18@gmail.com>
2026-05-27 07:07:22 +0530

    Introduced testng into framework
    added testng dependency to pom.xml file and installed plugin as well
    
    used @Test and @BeforeMethod annotation in LoginTest to make the method
    as test method
    
    added test-outut folder to .gitignore

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -1,15 +1,25 @@
 package com.ui.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import com.constants.Browser;
 import com.ui.pages.HomePage;
 
 public class LoginTest {
+	private HomePage homePage;
+	
+	@BeforeMethod(description = "Load the homepage of the website")
+	public void setUp() {
+		homePage = new HomePage(Browser.CHROME);
+	}
 
-	public static void main(String[] args) {
-
-		HomePage homePage = new HomePage(Browser.CHROME);
-		String username = homePage.goToLoginPage().doLoginWith("kawohip932@gzeos.com", "Pwd@123").getUserName();
-		System.out.println(username);
+	@Test(description = "Verifies if the valid user is able to login into the application", groups = { "sanity",
+			"smoke", "regression" })
+	public void loginTest() {
+		Assert.assertEquals(homePage.goToLoginPage().doLoginWith("kawohip932@gzeos.com", "Pwd@123").getUserName(),
+				"Chetan AG");
 	}
 
 }
